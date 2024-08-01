defe81ef1ce11b23d52a7ea73f973fcf3ca8b05d
Aquamarine444 <131270194+Aquamarine444@users.noreply.github.com>
2024-08-01 18:12:27 +0200

    Fix

--- ProjectFile/IndigoPark_Team3/Assets/Assets/UI Assets/UiGameManager.cs
+++ ProjectFile/IndigoPark_Team3/Assets/Assets/UI Assets/UiGameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class UiGameManager : MonoBehaviour
 {
     public GameObject PauseScreen;
     public string SceneName;
